2a47103d270b09b2317e6dc0d22ff9c25327e0fb
Knarkzel <knarkzel@gmail.com>
2023-08-31 22:19:41 +0200

    latest

--- default.nix
+++ default.nix
@@ -8,6 +8,7 @@
 , glib-networking
 , libappindicator
 , libayatana-appindicator
+, gst_all_1
 }:
 
 stdenv.mkDerivation rec {
@@ -30,6 +31,15 @@ stdenv.mkDerivation rec {
   buildInputs = [
     webkitgtk
     glib-networking
+
+    # some of these are necessary
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    # gst_all_1.gst-libav
+    # gst_all_1.gst-vaapi
   ];
 
   unpackCmd = "dpkg-deb -x $curSrc source";
